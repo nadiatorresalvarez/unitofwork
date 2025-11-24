0c713a2908c935fb0ef42fca886f474048bc87f1
Nadia Torres <nadia.torres@tecsup.edu.pe>
2025-11-23 21:22:44 -0500

    Update dbContextLab4.cs

--- Lab4-NadiaTorres/Models/dbContextLab4.cs
+++ Lab4-NadiaTorres/Models/dbContextLab4.cs
@@ -27,9 +27,9 @@ public partial class dbContextLab4 : DbContext
 
     public virtual DbSet<Producto> Productos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=tiendadb;Username=postgres;Password=tecsup");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=tiendadb;Username=postgres;Password=tecsup");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
